d3c29b300c2184fe1c522d979f746a9548edee33
vishalpatilmaske <vishalpatilmaske@gmail.com>
2026-02-28 22:21:19 +0530

    fix

--- src/pages/public/LandingPage.jsx
+++ src/pages/public/LandingPage.jsx
@@ -1,8 +1,13 @@
 import Footer from "../../components/common/Footer";
 import Navbar from "../../components/common/Navbar";
 import Hero from "../../components/landingpage/HeroSection";
-import HowItWorksSection from "../../components/landingpage/HowItWorksSection";
 import JobCategoriesSection from "../../components/landingpage/JobCategoriesSection";
+import FeaturedJobsSection from "../../components/landingpage/FeaturedJobsSection";
+import WhyChooseUsSection from "../../components/landingpage/WhyChooseUsSection";
+import EmployerBannerSection from "../../components/landingpage/EmployerBannerSection";
+import CoolPlacesSection from "../../components/landingpage/CoolPlacesSection";
+import CareerResourcesSection from "../../components/landingpage/CareerResourcesSection";
+import TestimonialsSection from "../../components/landingpage/TestimonialsSection";
 import CallToActionSection from "../../components/landingpage/CallToActionSection";
 
 const Landing = () => {
@@ -10,8 +15,13 @@ const Landing = () => {
     <>
       <Navbar />
       <Hero />
-      <HowItWorksSection />
       <JobCategoriesSection />
+      <FeaturedJobsSection />
+      <WhyChooseUsSection />
+      <EmployerBannerSection />
+      <CoolPlacesSection />
+      <CareerResourcesSection />
+      <TestimonialsSection />
       <CallToActionSection />
       <Footer />
     </>
